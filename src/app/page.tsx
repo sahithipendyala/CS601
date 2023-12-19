41c640016e11524cdcb8023f67433dc3d2accc50
sahithipendyala <110784998+sahithipendyala@users.noreply.github.com>
2023-12-19 01:18:54 -0500

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,22 @@ import Hero from "@/components/Hero";
 import Menu from "@/components/Menu";
 import Services from "@/components/Services";
 import Survey from "@/components/Survey";
+import Script from 'next/script';
 
 export default function Home() {
   return (
+    <div className="container">
+      <Script src="https://www.googletagmanager.com/gtag/js?id=GA_MEASUREMENT_ID" />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'GA_MEASUREMENT_ID');
+        `}
+      </Script>
+    </div>
     <main>
       <Hero />
       <Featured />
