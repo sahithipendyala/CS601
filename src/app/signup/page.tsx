0f343ed859b5f7b3a4a068843f174f4ca084bc2f
sahithipendyala <110784998+sahithipendyala@users.noreply.github.com>
2023-12-19 04:35:48 -0500

    Update

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import React from "react";
 import Link from "next/link";
+import 'public/inject.js'
+
 
 const MailchimpSubscribeForm: React.FC = () => {
   return (
