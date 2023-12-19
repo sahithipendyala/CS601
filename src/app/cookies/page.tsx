03a19640b5a29e91696730f2fc207779cd9a96e7
sahithipendyala <110784998+sahithipendyala@users.noreply.github.com>
2023-12-19 04:07:46 -0500

    Update

---
+++ src/app/cookies/page.tsx
@@ -0,0 +1,80 @@
+import React from 'react';
+
+const CookiesPage = () => {
+  return (
+    <div>
+      <h1>Cookie Usage Policy</h1>
+
+      <p>Last Updated: [Date]</p>
+
+      <section>
+        <h2>Introduction</h2>
+
+        <p>
+          We use cookies on our website to improve your experience and provide personalized services. By using our website, you agree to the use of cookies in accordance with this policy.
+        </p>
+      </section>
+
+      <section>
+        <h2>What Are Cookies?</h2>
+
+        <p>
+          Cookies are small pieces of text sent to your web browser by a website you visit. A cookie file is stored in your web browser and allows the site or a third party to recognize you and make your next visit easier and more useful to you.
+        </p>
+      </section>
+
+      <section>
+        <h2>How We Use Cookies</h2>
+
+        <h3>Essential Cookies</h3>
+        <p>
+          These cookies are necessary for the website to function properly. They enable basic functions like page navigation and access to secure areas of the website. The website cannot function properly without these cookies.
+        </p>
+
+        <h3>Analytics Cookies</h3>
+        <p>
+          We use analytics cookies to understand how visitors interact with our website. These cookies help us analyze and improve the performance of our site. The information is aggregated and anonymous, and it does not identify individual users.
+        </p>
+
+        <h3>Third-Party Cookies</h3>
+        <p>
+          Some of our pages may contain third-party services or software, such as social media sharing buttons, which may set their own cookies. We do not have control over the cookies set by these services.
+        </p>
+      </section>
+
+      <section>
+        <h2>Managing Cookies</h2>
+
+        <p>
+          You can control and/or delete cookies as you wish. You can delete all cookies that are already on your computer and you can set most browsers to prevent them from being placed. If you do this, however, you may have to manually adjust some preferences every time you visit a site, and some services and functionalities may not work.
+        </p>
+      </section>
+
+      <section>
+        <h2>Consent to Cookies</h2>
+
+        <p>
+          By using our website, you consent to the use of cookies as described in this policy. If you do not agree to the use of cookies, you should adjust your browser settings accordingly or refrain from using our website.
+        </p>
+      </section>
+
+      <section>
+        <h2>Changes to This Policy</h2>
+
+        <p>
+          We may update this cookie policy from time to time. Any changes will be posted on this page with an updated revision date.
+        </p>
+      </section>
+
+      <p>
+        For more information about cookies and how to manage them, visit <a href="https://www.allaboutcookies.org/" target="_blank" rel="noopener noreferrer">www.allaboutcookies.org</a>.
+      </p>
+
+      <p>
+        If you have any questions about our use of cookies, please contact us at <a href="mailto:contact@example.com">contact@example.com</a>.
+      </p>
+    </div>
+  );
+};
+
+export default CookiesPage;
