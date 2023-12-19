30946b18597f28b56668d59548200c4b832fafda
sahithipendyala <110784998+sahithipendyala@users.noreply.github.com>
2023-12-19 02:58:56 -0500

    Update

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,21 @@
+// components/Signup.js
+import MailchimpForm from "@/components/MailchimpForm";
+import Link from "next/link";
+
+const Signup = () => {
+  return (
+    <div style={{ fontFamily: 'Arial, sans-serif', backgroundColor: '#f4f4f4', margin: 0, padding: 0, display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
+
+      <div style={{ backgroundColor: '#fff', borderRadius: '8px', boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)', padding: '20px', width: '300px' }}>
+
+        <form >
+            <MailchimpForm/>
+        </form>
+        
+      </div>
+
+    </div>
+  );
+};
+
+export default Signup;
