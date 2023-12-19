fbb32bb35eb70dd0ae40e8a613e3c88d136f0045
sahithipendyala <110784998+sahithipendyala@users.noreply.github.com>
2023-12-19 03:21:25 -0500

    Update

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,21 +1,102 @@
 // components/Signup.js
-import MailchimpForm from "@/components/MailchimpForm";
+// import MailchimpForm from "@/components/MailchimpForm";
 import Link from "next/link";
+import React from 'react';
 
-const Signup = () => {
-  return (
-    <div style={{ fontFamily: 'Arial, sans-serif', backgroundColor: '#f4f4f4', margin: 0, padding: 0, display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
-
-      <div style={{ backgroundColor: '#fff', borderRadius: '8px', boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)', padding: '20px', width: '300px' }}>
-
-        <form >
-            <MailchimpForm/>
-        </form>
-        
-      </div>
-
+const Signup = () =>  (
+  <div id="mc_embed_shell">
+    <style jsx>{`
+      #mc_embed_signup {
+        background: #fff;
+        clear: left;
+        font: 14px Helvetica, Arial, sans-serif;
+        width: 600px;
+      }
+      /* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
+           We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+    `}</style>
+    <div id="mc_embed_signup">
+      <form
+        action="https://app.us10.list-manage.com/subscribe/post?u=9c6c5523b284c176465ff6c9e&amp;id=8f58fa7c66&amp;f_id=0007cde5f0"
+        method="post"
+        id="mc-embedded-subscribe-form"
+        name="mc-embedded-subscribe-form"
+        className="validate"
+        target="_blank"
+      >
+        <div id="mc_embed_signup_scroll">
+          <h2>Subscribe</h2>
+          <div className="indicates-required">
+            <span className="asterisk">*</span> indicates required
+          </div>
+          <div className="mc-field-group">
+            <label htmlFor="mce-EMAIL">Email Address <span className="asterisk">*</span></label>
+            <input type="email" name="EMAIL" className="required email" id="mce-EMAIL" required="" value="" />
+          </div>
+          <div className="mc-field-group">
+            <label htmlFor="mce-FNAME">First Name </label>
+            <input type="text" name="FNAME" className="text" id="mce-FNAME" value="" />
+          </div>
+          <div className="mc-field-group">
+            <label htmlFor="mce-LNAME">Last Name </label>
+            <input type="text" name="LNAME" className="text" id="mce-LNAME" value="" />
+          </div>
+          <div id="mce-responses" className="clear foot">
+            <div className="response" id="mce-error-response" style={{ display: 'none' }}></div>
+            <div className="response" id="mce-success-response" style={{ display: 'none' }}></div>
+          </div>
+          <div aria-hidden="true" style={{ position: 'absolute', left: '-5000px' }}>
+            {/* real people should not fill this in and expect good things - do not remove this or risk form bot signups */}
+            <input type="text" name="b_9c6c5523b284c176465ff6c9e_8f58fa7c66" tabIndex="-1" value="" />
+          </div>
+          <div className="optionalParent">
+            <div className="clear foot">
+              <input
+                type="submit"
+                name="subscribe"
+                id="mc-embedded-subscribe"
+                className="button"
+                value="Subscribe"
+              />
+              <p style={{ margin: '0px auto' }}>
+                <a
+                  href="http://eepurl.com/iGu5Yo"
+                  title="Mailchimp - email marketing made easy and fun"
+                >
+                  <span style={{ display: 'inline-block', backgroundColor: 'transparent', borderRadius: '4px' }}>
+                    <img
+                      className="refferal_badge"
+                      src="https://digitalasset.intuit.com/render/content/dam/intuit/mc-fe/en_us/images/intuit-mc-rewards-text-dark.svg"
+                      alt="Intuit Mailchimp"
+                      style={{
+                        width: '220px',
+                        height: '40px',
+                        display: 'flex',
+                        padding: '2px 0px',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                      }}
+                    />
+                  </span>
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </form>
     </div>
-  );
-};
+    <script
+      type="text/javascript"
+      src="//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js"
+    ></script>
+    <script
+      type="text/javascript"
+      dangerouslySetInnerHTML={{
+        __html: `(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[3]='ADDRESS';ftypes[3]='address';fnames[4]='PHONE';ftypes[4]='phone';fnames[5]='BIRTHDAY';ftypes[5]='birthday';}(jQuery));var $mcj = jQuery.noConflict(true);`,
+      }}
+    ></script>
+  </div>
+);
+
 
 export default Signup;
